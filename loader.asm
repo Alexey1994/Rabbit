b4d0cd881a23d3e04e48c67ccd954ea696a7ca2e
Alexey1994 <alexeyborisenko20@gmail.com>
2016-08-13 03:56:56 +0300

    добавлена работа с диском

--- loader.asm
+++ loader.asm
@@ -2,13 +2,71 @@ use16
 org  0x7C00
 start:
 
-call print
-call print
-call print
+;call print_string
+;call print_string
+;call print_string
+
+;mov ax, 0
+;call print_byte
+
+;ret
+
+call load_kernel
+
+mov al, [0x7e00]
+call print_byte
+
+mov al, ' '
+call print_character
+mov al, [0x7e01]
+call print_byte
+
+mov al, ' '
+call print_character
+mov al, [0x7e02]
+call print_byte
+
+mov al, ' '
+call print_character
+mov al, [0x7e03]
+call print_byte
+
+mov al, ' '
+call print_character
+mov al, [0x7e04]
+call print_byte
+
+mov al, ' '
+call print_character
+mov al, [0x7e05]
+call print_byte
+
+mov al, ' '
+call print_character
+mov al, [0x7e06]
+call print_byte
+
+mov al, ' '
+call print_character
+mov al, [0x7e07]
+call print_byte
+
+mov al, ' '
+call print_character
+mov al, [0x7e08]
+call print_byte
+
+mov al, ' '
+call print_character
+mov al, 208
+call print_byte
 
 jmp $
 
+ret
+
 %include "print.asm"
+%include "disk.asm"
 
 finish:
     times 0x1FE-finish+start db 0
